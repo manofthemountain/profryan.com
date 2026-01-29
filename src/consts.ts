cf3dad5f3139c6c3e41ea7d1d3591dec713639f1
manofthemountain <67065684+manofthemountain@users.noreply.github.com>
2026-01-29 14:34:36 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Prof Ryan | Practical Cybersecurity";
+export const SITE_DESCRIPTION = "A practical cybersecurity blog focused on systems, networks, and real-world defense.";
